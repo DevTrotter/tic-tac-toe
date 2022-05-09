512c42e636aaed78307670df28e46604d253e8e3
Devtrotter <sebastienchapuy19@gmail.com>
2022-05-09 16:26:45 +0200

    [#20] add win algo

--- src/Components/Box/Box.js
+++ src/Components/Box/Box.js
@@ -3,19 +3,28 @@ import { StyledBox } from "./StyledBox";
 import { ImCross } from "react-icons/im";
 import { BsFillRecordCircleFill } from "react-icons/bs";
 
-export const Box = ({ player, setPlayground, boxNumber, setPlayer }) => {
+export const Box = ({
+  player,
+  setPlayground,
+  boxNumber,
+  setPlayer,
+  playground,
+  checkWin,
+}) => {
   const [value, setValue] = useState(null);
 
   const handleClick = () => {
-    if (player === "x") {
-      setPlayground[boxNumber] = "x";
-      setValue(<ImCross />);
-      setPlayer("o");
-    } else {
-      setPlayground[boxNumber] = "o";
-      setValue(<BsFillRecordCircleFill />);
-      setPlayer("x");
-    }
+    const newPlayground = playground.map((box, i) =>
+      i === boxNumber ? player : box
+    );
+    const icon = player === "x" ? <ImCross /> : <BsFillRecordCircleFill />;
+    setValue(icon);
+    setPlayer(player === "x" ? "o" : "x");
+    setPlayground(newPlayground);
+    const playerWin = checkWin(player, newPlayground);
+    setTimeout(() => {
+      if (playerWin) alert(`Player ${player.toUpperCase()} is win !`);
+    }, 1000);
   };
 
   return <StyledBox onClick={handleClick}>{value}</StyledBox>;
--- src/Components/BoxList/BoxList.js
+++ src/Components/BoxList/BoxList.js
@@ -2,19 +2,39 @@ import React, { useState } from "react";
 import { Box } from "../Box/Box";
 import { StyledBoxList } from "./StyledBoxList";
 
+const checkWin = (player, playground) => {
+  return (
+    (playground[0] === player &&
+      playground[3] === player &&
+      playground[6] === player) ||
+    (playground[1] === player &&
+      playground[4] === player &&
+      playground[7] === player) ||
+    (playground[2] === player &&
+      playground[5] === player &&
+      playground[8] === player) ||
+    (playground[0] === player &&
+      playground[4] === player &&
+      playground[8] === player) ||
+    (playground[2] === player &&
+      playground[4] === player &&
+      playground[6] === player) ||
+    (playground[0] === player &&
+      playground[1] === player &&
+      playground[2] === player) ||
+    (playground[3] === player &&
+      playground[4] === player &&
+      playground[5] === player) ||
+    (playground[6] === player &&
+      playground[7] === player &&
+      playground[8] === player)
+  );
+};
+
 export const BoxList = () => {
-  const [playground, setPlayground] = useState([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
+  const [playground, setPlayground] = useState(Array(9).fill(""));
   const [player, setPlayer] = useState("x");
+
   return (
     <StyledBoxList>
       {playground?.map((box, boxNumber) => (
@@ -22,8 +42,10 @@ export const BoxList = () => {
           key={boxNumber}
           player={player}
           setPlayer={setPlayer}
+          playground={playground}
           setPlayground={setPlayground}
           boxNumber={boxNumber}
+          checkWin={checkWin}
         />
       ))}
     </StyledBoxList>
